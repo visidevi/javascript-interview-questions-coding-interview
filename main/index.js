a882f20238ebf6b1cb3a494547ca3295222f23fc
Visaka Devi <vdevi@commvault.com>
2022-08-20 11:03:25 -0400

    Map, Sort, Filter

---
+++ main/index.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 40,
+    email: "michael.lawson@reqres.in",
+    first_name: "Michael",
+    last_name: "Lawson",
+    avatar: "https://reqres.in/img/faces/7-image.jpg",
+    active: true,
+  },
+  {
+    id: 20,
+    email: "lindsay.ferguson@reqres.in",
+    first_name: "Lindsay",
+    last_name: "Ferguson",
+    avatar: "https://reqres.in/img/faces/8-image.jpg",
+    active: true,
+  },
+  {
+    id: 30,
+    email: "tobias.funke@reqres.in",
+    first_name: "Tobias",
+    last_name: "Funke",
+    avatar: "https://reqres.in/img/faces/9-image.jpg",
+    active: true,
+  },
+];
+
+const getFirstsName = () => {
+  /** Sort mutate the array itself */
+  return data
+    .sort((a, b) => (a.id < b.id ? -1 : 1))
+    .filter((user) => user.active)
+    .map((user) => user.first_name);
+};
+
+console.log(`getFirstsName():------>`, getFirstsName());
+
+
+/**
+ * Difference between null and undefined
+ */
+
+let a;
+console.log("a", "value:", a ,"type:",typeof a)
+a = null;
+console.log("a", "value:", a ,"type:",typeof a)
---
+++ main/map.js
@@ -0,0 +1,30 @@
+const data = [
+    {
+        "id": 7,
+        "email": "michael.lawson@reqres.in",
+        "first_name": "Michael",
+        "last_name": "Lawson",
+        "avatar": "https://reqres.in/img/faces/7-image.jpg",
+        "active": true,
+    },
+    {
+        "id": 8,
+        "email": "lindsay.ferguson@reqres.in",
+        "first_name": "Lindsay",
+        "last_name": "Ferguson",
+        "avatar": "https://reqres.in/img/faces/8-image.jpg",
+        "active": true,
+    },
+    {
+        "id": 9,
+        "email": "tobias.funke@reqres.in",
+        "first_name": "Tobias",
+        "last_name": "Funke",
+        "avatar": "https://reqres.in/img/faces/9-image.jpg",
+        "active": false,
+    }
+]
+
+export  const getFirstsName =() => {
+    return data.filter(user=> user.active).map(user => user.first_name)
+}
